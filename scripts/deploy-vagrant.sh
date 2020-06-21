ddf4bc4e87118be079efa2f890592986c5d3e7d9
githubfoam <lequestionis@gmail.com>
2020-06-21 15:56:58 +0300

    то́ же

--- scripts/deploy-vagrant.sh
+++ scripts/deploy-vagrant.sh
@@ -21,17 +21,16 @@ echo "=============================Install Vagrant==============================
 vagrant plugin install vagrant-libvirt #The vagrant-libvirt plugin is required when using KVM on Linux
 vagrant plugin install vagrant-mutate #Convert vagrant boxes to work with different providers
 
-# # https://app.vagrantup.com/opensuse/boxes/Tumbleweed.x86_64
-# vagrant box add  "opensuse/Tumbleweed.x86_64" --provider=libvirt
-# vagrant up --provider=libvirt "vg-opensuse-01"
-# # https://app.vagrantup.com/opensuse/boxes/Kubic.x86_64
-# vagrant box add  "opensuse/Kubic.x86_64" --provider=libvirt
-# vagrant up --provider=libvirt "vg-opensusekubicx8664"
-# # https://app.vagrantup.com/opensuse/boxes/MicroOS.x86_64
-# vagrant box add  "opensuse/MicroOS.x86_64" --provider=libvirt
-# vagrant up --provider=libvirt "vg-microosx8664"
-
-# # https://github.com/chef/bento/tree/master/packer_templates/opensuse
+# https://app.vagrantup.com/opensuse/boxes/Tumbleweed.x86_64
+vagrant box add  "opensuse/Tumbleweed.x86_64" --provider=libvirt
+vagrant up --provider=libvirt "vg-opensuse-01"
+# https://app.vagrantup.com/opensuse/boxes/Kubic.x86_64
+vagrant box add  "opensuse/Kubic.x86_64" --provider=libvirt
+vagrant up --provider=libvirt "vg-opensusekubicx8664"
+# https://app.vagrantup.com/opensuse/boxes/MicroOS.x86_64
+vagrant box add  "opensuse/MicroOS.x86_64" --provider=libvirt
+vagrant up --provider=libvirt "vg-microosx8664"
+# https://github.com/chef/bento/tree/master/packer_templates/opensuse
 vagrant box add  "bento/opensuse-leap-15.1" --provider=virtualbox
 vagrant mutate "bento/opensuse-leap-15.1"  libvirt
 vagrant up --provider=libvirt "vg-opensuseleap"
@@ -48,11 +47,6 @@ vagrant up --provider=libvirt "vg-opensuseleap"
 # # https://app.vagrantup.com/opensuse/boxes/Tumbleweed.aarch64
 # vagrant box add  "opensuse/Tumbleweed.aarch64" --provider=libvirt
 # vagrant up --provider=libvirt "vg-tumbleweedaarch64"
-
-
-
-
-
 
 vagrant status
 virsh list --all #show all running KVM/libvirt VMs
