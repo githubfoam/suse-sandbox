a8d9b0cc023851c48b7aa2980476655c5e35c292
githubfoam <lequestionis@gmail.com>
2020-06-28 13:50:19 +0300

    в`идимых

--- provisioning/susegeneric.sh
+++ provisioning/susegeneric.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-set -eox pipefail #safety for script
+set -o errexit
+set -o pipefail
+set -o nounset
+set -o xtrace
+# set -eox pipefail #safety for script
 
 echo "===================================================================================="
                           hostnamectl status
@@ -9,30 +13,4 @@ echo "          \  (oo)\_______                                                 
 echo "             (__)\       )\/\                                                      "
 echo "                 ||----w |                                                         "
 echo "                 ||     ||                                                         "
-echo "=============================ansible ============================================================="
-# hostnamectl set-hostname vg-arch-01
-# echo "192.168.18.81 vg-arch-01.local vg-arch-01" |sudo tee -a /etc/hosts
-# cat /etc/hosts
-# echo "name: nameserver, ip: 8.8.8.8 " |sudo tee -a /etc/resolv.conf
-# cat /etc/resolv.conf
-# pacman -Syu --noconfirm #Update and upgrade  all packages on the system
-# pacman -Si ansible #Show extensive info about a package
-# sudo pacman -Syu --noconfirm ansible #Install (and update package list)
-# # pacman -Ql ansible #List files installed by ansible package
-# ansible --version
-# cat <<EOF | sudo tee ansible-local.yml
-# ---
-#   - name: "Run ansible locally localhost"
-#     hosts: localhost
-#     connection: local
-#     tasks:
-#
-#     - name: "run ls -lrt command"
-#       shell: "ls -lrt"
-#       register: "output"
-#
-#     - debug: var=output.stdout_lines
-# EOF
-# pwd && ls -lai
-# ansible-playbook ansible-local.yml
-echo "=========================================================================================="
+echo "==================================================================================="
---
+++ scripts/deploy-boxes.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -o errexit
+set -o pipefail
+set -o nounset
+set -o xtrace
+# set -eox pipefail #safety for script
+
+echo "========================================================================================="
+vagrant plugin install vagrant-libvirt #The vagrant-libvirt plugin is required when using KVM on Linux
+vagrant plugin install vagrant-mutate #Convert vagrant boxes to work with different providers
+
+
+# https://app.vagrantup.com/opensuse/boxes/Tumbleweed.x86_64
+# vagrant box add  "opensuse/Tumbleweed.x86_64" --provider=libvirt
+# vagrant up --provider=libvirt "vg-opensuse-01"
+
+# https://app.vagrantup.com/opensuse/boxes/Kubic.x86_64
+vagrant box add  "opensuse/Kubic.x86_64" --provider=libvirt
+vagrant up --provider=libvirt "vg-opensusekubicx8664"
+
+# https://app.vagrantup.com/opensuse/boxes/MicroOS.x86_64
+# vagrant box add  "opensuse/MicroOS.x86_64" --provider=libvirt
+# vagrant up --provider=libvirt "vg-microosx8664"
+
+# https://github.com/chef/bento/tree/master/packer_templates/opensuse
+# vagrant box add  "bento/opensuse-leap-15.1" --provider=virtualbox
+# vagrant mutate "bento/opensuse-leap-15.1"  libvirt
+# vagrant up --provider=libvirt "vg-opensuseleap"
+
+
+vagrant box list #veridy installed boxes
+vagrant status #Check the status of the VMs to see that none of them have been created yet
+vagrant status
+virsh list --all #show all running KVM/libvirt VMs
