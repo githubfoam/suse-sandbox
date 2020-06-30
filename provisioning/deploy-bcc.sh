826306a0c1ad81b3d87f1d5eaf03f70578af64e6
githubfoam <lequestionis@gmail.com>
2020-07-01 01:34:37 +0300

    как мо́жно лу́чший

--- provisioning/deploy-bcc.sh
+++ provisioning/deploy-bcc.sh
@@ -11,8 +11,9 @@ echo "=============================bcc==========================================
 #https://github.com/iovisor/bcc/blob/master/INSTALL.md#opensuse---binary
 zypper ref #Refreshing Repositories
 zypper update -y #update all installed packages with newer version as far as possible
-#zypper -n bcc-tools bcc-examples #use --non-interactive mode, in shortcut -n
-zypper install -y --quiet bcc-tools bcc-examples git
+
+#Reboot is required to ensure that your system benefits from these updates.
+zypper install -y bcc-tools bcc-examples git
 
 #https://github.com/iovisor/bcc
 git clone https://github.com/iovisor/bcc.git && cd examples/tracing
