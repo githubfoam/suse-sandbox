46b45e6bc6271c2e2905e1d82897e702e22fd8b7
githubfoam <lequestionis@gmail.com>
2020-07-01 00:21:10 +0300

    typo

--- provisioning/deploy-bcc.sh
+++ provisioning/deploy-bcc.sh
@@ -10,7 +10,7 @@ echo "=============================bcc==========================================
 
 #https://github.com/iovisor/bcc/blob/master/INSTALL.md#opensuse---binary
 zypper ref #Refreshing Repositories
-zypper update --quiet #update all installed packages with newer version as far as possible
+zypper update #update all installed packages with newer version as far as possible
 #zypper -n bcc-tools bcc-examples #use --non-interactive mode, in shortcut -n
 zypper install -y --quiet bcc-tools bcc-examples git
 
