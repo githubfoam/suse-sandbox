8b070dc957614e5f88da3002fff184416ad6f0c4
githubfoam <lequestionis@gmail.com>
2020-06-30 22:42:53 +0300

    тем лу́чше

--- provisioning/deploy-bcc.sh
+++ provisioning/deploy-bcc.sh
@@ -10,7 +10,8 @@ echo "=============================bcc==========================================
 
 #https://github.com/iovisor/bcc/blob/master/INSTALL.md#opensuse---binary
 zypper ref
-zypper in bcc-tools bcc-examples
+#zypper -n bcc-tools bcc-examples #use --non-interactive mode, in shortcut -n
+zypper install -y bcc-tools bcc-examples
 
 #https://github.com/iovisor/bcc
 git clone https://github.com/iovisor/bcc.git && cd examples/tracing
