462f0dc79c6b640d10ff48ac1ca1564fba4d52b1
githubfoam <lequestionis@gmail.com>
2020-07-01 01:56:43 +0300

    иностр`анца

--- provisioning/deploy-bcc.sh
+++ provisioning/deploy-bcc.sh
@@ -16,5 +16,5 @@ zypper update -y #update all installed packages with newer version as far as pos
 zypper install -y bcc-tools bcc-examples git
 
 #https://github.com/iovisor/bcc
-git clone https://github.com/iovisor/bcc.git && cd examples/tracing
+git clone https://github.com/iovisor/bcc.git && cd bcc/examples/tracing
 ./bitehist.py
